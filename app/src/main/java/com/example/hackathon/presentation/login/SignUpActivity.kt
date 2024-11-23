c5e7c533f2dd0df41850315b65e6cf54a3b491f2
Yehyun Jo <81362348+JoYehyun99@users.noreply.github.com>
2024-11-24 02:01:20 +0900

    [Feat] Login, SignUp, Start 화면 (#9)
    * feat: 로그인 화면 구성 수정
    
    * feat: 회원가입 화면 구성 수정
    
    * feat: 시작 화면 수정
    
    * chore: 코루틴 gradle 설정
    
    * chore: 불필요한 파일 삭제
    
    * chore: 코드 정리

--- app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
+++ app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
@@ -3,19 +3,21 @@ package com.example.hackathon.presentation.login
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.hackathon.R
+import com.example.hackathon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.btnLogin.setOnClickListener {
+            // 회원가입 완료?
         }
     }
 }
--- app/src/main/java/com/example/hackathon/presentation/login/StartActivity.kt
+++ app/src/main/java/com/example/hackathon/presentation/login/StartActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.hackathon.R
 
 class StartActivity : AppCompatActivity() {
@@ -14,21 +12,16 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val loginButton = findViewById<ImageButton>(R.id.btn_start_login)
         loginButton.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
         val signUpButton = findViewById<ImageButton>(R.id.btn_start_signup)
-        signUpButton.setOnClickListener{
-            val intent = Intent(this,SignUpActivity::class.java)
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
     }
