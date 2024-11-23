82029cb07f833daf98c8816c013f0a00383e7cd2
BAEK0111 <baekjw111@gmail.com>
2024-11-24 07:13:24 +0900

    fix : activity -> fragment 수정

--- app/src/main/java/com/example/hackathon/presentation/groupdetail/GroupDetailQuizFragment.kt
+++ app/src/main/java/com/example/hackathon/presentation/groupdetail/GroupDetailQuizFragment.kt
@@ -12,35 +12,14 @@ import com.example.hackathon.R
 import com.example.hackathon.presentation.model.User
 
 class GroupDetailQuizFragment : Fragment() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: GroupDetailQuizAdapter
-
-    private val dummyUserList = listOf(
-        User(id = "1", name = "로빈"),
-        User(id = "2", name = "신구"),
-        User(id = "3", name = "똘이"),
-        User(id = "4", name = "상우")
-    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_group_detail_quiz, container, false)
-
-        recyclerView = view.findViewById(R.id.recycler_group_detail_quiz_answer_list)
-        setupRecyclerView()
-
         return view
     }
 
-    private fun setupRecyclerView() {
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        adapter = GroupDetailQuizAdapter(dummyUserList) { user ->
-            // 클릭 이벤트 처리
-            println("Clicked user: ${user.name}")
-        }
-        recyclerView.adapter = adapter
-    }
 
 }
