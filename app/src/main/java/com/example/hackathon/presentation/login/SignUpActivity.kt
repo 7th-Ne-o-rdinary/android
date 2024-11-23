d2a6c4488c46b2679e3c9738dcee56628a05a74d
BAEK0111 <baekjw111@gmail.com>
2024-11-24 00:10:37 +0900

    feat : 로그인, 회원가입 UI, clickEvent

--- app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
+++ app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
@@ -1,29 +1,21 @@
 package com.example.hackathon.presentation.login
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hackathon.R
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val signUpTv = findViewById<TextView>(R.id.tv_login_signup_btn)
-        signUpTv.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java) // 이동할 액티비티를 설정
-            startActivity(intent)
         }
     }
 }
