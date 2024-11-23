90454027503e1b3e001d3a8b5cd05beb85db28ed
JoYehyun <jo990714@naver.com>
2024-11-23 22:04:26 +0900

    fix: 에러 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,17 @@ plugins {
 
 android {
     namespace = "com.example.hackathon"
-    compileSdk = 34
+    compileSdk = 35
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 
     defaultConfig {
         applicationId = "com.example.hackathon"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
