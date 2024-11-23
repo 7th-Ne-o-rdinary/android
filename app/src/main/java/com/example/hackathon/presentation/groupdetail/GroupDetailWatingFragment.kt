0aef2e83806e86d2ff2ad08bd06af5dc70c59329
isumin <leesumin0526@gmail.com>
2024-11-23 20:32:32 +0900

    [Feat] Xml 작업 완료

---
+++ app/src/main/java/com/example/hackathon/presentation/groupdetail/GroupDetailWatingFragment.kt
@@ -0,0 +1,17 @@
+package com.example.hackathon.presentation.groupdetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GroupDetailWatingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GroupDetailWatingFragment : Fragment() {
+
+}
