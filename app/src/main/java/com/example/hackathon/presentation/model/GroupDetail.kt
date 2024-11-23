c7473953fea1a83a352e695c4ba932cf56d904b0
JoYehyun <jo990714@naver.com>
2024-11-24 00:39:02 +0900

    feat: 데이터 수정

--- app/src/main/java/com/example/hackathon/presentation/model/GroupDetail.kt
+++ app/src/main/java/com/example/hackathon/presentation/model/GroupDetail.kt
@@ -2,8 +2,8 @@ package com.example.hackathon.presentation.model
 
 // 방에 대한 자세한 정보들을 얻는다.
 data class GroupDetail(
-    val groupLeaderId: Int,
+    val code: String,
+    val groupLeader: String,
     val groupName: String,
     val prizeList: List<Prize>, // 상 목록
-    val code: String,
 )
