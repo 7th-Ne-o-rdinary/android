8a0bbd3bdb77514ba9b3e1874012dcc4569869ba
JoYehyun <jo990714@naver.com>
2024-11-24 02:00:21 +0900

    chore: 코드 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,4 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-retrofit:1.7.3")
 }
