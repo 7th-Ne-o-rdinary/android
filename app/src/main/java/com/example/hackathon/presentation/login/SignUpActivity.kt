8e2cab5e097c13409c034a93685b754f5e746650
JoYehyun <jo990714@naver.com>
2024-11-24 01:21:42 +0900

    feat: 회원가입 화면 구성 수정

--- app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
+++ app/src/main/java/com/example/hackathon/presentation/login/SignUpActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hackathon.R
+import com.example.hackathon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
+
+        binding.btnLogin.setOnClickListener {
+            // 회원가입 완료?
+        }
     }
 }
