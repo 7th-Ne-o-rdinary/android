7da108733641df2cb9ac7f4ce0c0bef89e44b240
BAEK0111 <baekjw111@gmail.com>
2024-11-23 20:33:02 +0900

    feat : UI 완료

---
+++ app/src/main/java/com/example/hackathon/data/User.kt
@@ -0,0 +1,8 @@
+package com.example.hackathon.data
+
+data class User(
+    val id : Int,
+    val email : String,
+    val password : String,
+    val name : String
+)
---
+++ app/src/main/java/com/example/hackathon/presentation/grouplist/GroupListActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hackathon.presentation.grouplist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hackathon.R
+
+class GroupListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_group_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
