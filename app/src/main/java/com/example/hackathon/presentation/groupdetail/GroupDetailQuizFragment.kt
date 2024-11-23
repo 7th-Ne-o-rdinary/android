ccee54bf31b4b60f2a6af944f49cace447757825
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-11-23 20:36:06 +0900

    [Feat] Xml 작업 완료 (#2)

---
+++ app/src/main/java/com/example/hackathon/presentation/groupdetail/GroupDetailQuizFragment.kt
@@ -0,0 +1,19 @@
+package com.example.hackathon.presentation.groupdetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.example.hackathon.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GroupDetailQuizFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GroupDetailQuizFragment : Fragment() {
+
+
+}
